9d35f3d36ba7f8646a40286183944f55b4ecc333
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2024-01-25 12:18:04 -0500

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i < 11 and i > 0:
+        print(i)
+        i -= 1
+        print('Happy New Year!')
+
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    square_int = [int * int for int in int_list]
+    return square_int
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if not i % 15:
+            print("FizzBuzz")
+        elif not i % 5:
+            print("Buzz")
+        elif not i % 3:
+            print("Fizz")
+        else:
+            print(i)
